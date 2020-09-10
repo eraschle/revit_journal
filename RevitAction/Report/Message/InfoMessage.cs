02b9185007b5f17c987d6cf4ce60497e32bace19
eraschle <erichraschle@gmail.com>
2020-09-11 00:03:04 +0200

    removed unused classes

--- RevitAction/Report/Message/InfoMessage.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace RevitAction.Report.Message
-{
-    public class InfoMessage : AReportMessage, IReportMessage
-    {
-        public InfoMessage() : base(ReportKind.Info) { }
-        public InfoMessage(string message) : base(ReportKind.Info, message) { }
-    }
-}
