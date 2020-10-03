f4627f5dd0702b93f12c0a01df0a047972904d60
eraschle <erichraschle@gmail.com>
2020-10-04 00:03:59 +0200

    added Levensthein class to Utilities project

--- RevitJournal/Duplicate/Comparer/LevenstheinHelper.cs
+++ RevitJournal/Duplicate/Comparer/LevenstheinHelper.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.Globalization;
+using Utilities.Helper;
 
 namespace RevitJournal.Duplicate.Comparer
 {
@@ -6,57 +7,12 @@ namespace RevitJournal.Duplicate.Comparer
     {
         public static string LevenstheinAsString(int distance)
         {
-            if (distance == 0) { return string.Empty; }
-            return distance.ToString();
+            return distance == 0 ? string.Empty : distance.ToString(CultureInfo.CurrentCulture);
         }
 
         public static int ComputeLevensthein(string original, string other)
         {
-            if (IsBooleanValue(original) && IsBooleanValue(other))
-            {
-                var originalBool = bool.Parse(original);
-                var otherBool = bool.Parse(other);
-                if (originalBool != otherBool) { return 1; }
-
-                return 0;
-            }
-
-            var originalLength = original.Length;
-            var otherLength = other.Length;
-            var twoDArray = new int[originalLength + 1, otherLength + 1];
-
-            // Step 1
-            if (originalLength == 0) { return otherLength; }
-            if (otherLength == 0) { return originalLength; }
-
-            // Step 2
-            for (var origIdx = 0; origIdx <= originalLength; twoDArray[origIdx, 0] = origIdx++) { }
-            for (var otherIdx = 0; otherIdx <= otherLength; twoDArray[0, otherIdx] = otherIdx++) { }
-
-            // Step 3
-            for (var origIdx = 1; origIdx <= originalLength; origIdx++)
-            {
-                //Step 4
-                for (var otherIdx = 1; otherIdx <= otherLength; otherIdx++)
-                {
-                    // Step 5
-                    var cost = (other[otherIdx - 1] == original[origIdx - 1]) ? 0 : 1;
-
-                    // Step 6
-                    twoDArray[origIdx, otherIdx] = Math.Min(
-                        Math.Min(twoDArray[origIdx - 1, otherIdx] + 1, twoDArray[origIdx, otherIdx - 1] + 1),
-                        twoDArray[origIdx - 1, otherIdx - 1] + cost);
-                }
-            }
-            // Step 7
-            return twoDArray[originalLength, otherLength];
-        }
-
-        private static bool IsBooleanValue(string value)
-        {
-            return string.IsNullOrEmpty(value) == false
-                && (value.Equals(bool.TrueString, StringComparison.CurrentCultureIgnoreCase)
-                || value.Equals(bool.FalseString, StringComparison.CurrentCultureIgnoreCase));
+            return Levensthein.GetDistance(original, other);
         }
     }
 }
---
+++ Utilities/Helper/Levensthein.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Utilities.System;
+
+namespace Utilities.Helper
+{
+    public static class Levensthein
+    {
+        public static int GetDistance(string original, string other)
+        {
+            if (IsBooleanValue(original) && IsBooleanValue(other))
+            {
+                var originalBool = bool.Parse(original);
+                var otherBool = bool.Parse(other);
+                return originalBool != otherBool ? 1 : 0;
+            }
+
+            var originalLength = original.Length;
+            var otherLength = other.Length;
+            var twoDArray = new int[originalLength + 1, otherLength + 1];
+
+            // Step 1
+            if (originalLength == 0) { return otherLength; }
+            if (otherLength == 0) { return originalLength; }
+
+            // Step 2
+            for (var origIdx = 0; origIdx <= originalLength; twoDArray[origIdx, 0] = origIdx++) { }
+            for (var otherIdx = 0; otherIdx <= otherLength; twoDArray[0, otherIdx] = otherIdx++) { }
+
+            // Step 3
+            for (var origIdx = 1; origIdx <= originalLength; origIdx++)
+            {
+                //Step 4
+                for (var otherIdx = 1; otherIdx <= otherLength; otherIdx++)
+                {
+                    // Step 5
+                    var cost = other[otherIdx - 1] == original[origIdx - 1] ? 0 : 1;
+
+                    // Step 6
+                    twoDArray[origIdx, otherIdx] = Math.Min(
+                        Math.Min(twoDArray[origIdx - 1, otherIdx] + 1, twoDArray[origIdx, otherIdx - 1] + 1),
+                        twoDArray[origIdx - 1, otherIdx - 1] + cost);
+                }
+            }
+            // Step 7
+            return twoDArray[originalLength, otherLength];
+        }
+
+        private static bool IsBooleanValue(string value)
+        {
+            return string.IsNullOrEmpty(value) == false
+                && (StringUtils.Equals(value, bool.TrueString)
+                   || StringUtils.Equals(value, bool.FalseString));
+        }
+    }
+}
