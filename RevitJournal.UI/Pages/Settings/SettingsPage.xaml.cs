6312b1f4c05285599dde80637ff8dc4877bd8d57
eraschle <erichraschle@gmail.com>
2020-10-06 00:44:07 +0200

    removed unused methods

--- RevitJournal.UI/Pages/Settings/SettingsPage.xaml.cs
+++ RevitJournal.UI/Pages/Settings/SettingsPage.xaml.cs
@@ -8,35 +8,10 @@ namespace RevitJournalUI.Pages.Settings
     /// </summary>
     public partial class SettingsPage : Page
     {
-        private const string prefixTimeoutTitle = "Timeout";
-        private const string prefixParallelProcess = "Parallel Processes";
-
-        //private SettingsPageModel ViewModel
-        //{
-        //    get { return DataContext as SettingsPageModel; }
-        //}
-        
         public SettingsPage()
         {
             InitializeComponent();
         }
 
-        private void OnTimeoutUpdated(object sender, DataTransferEventArgs args)
-        {
-            //if (!(sender is Label label)) { return; }
-
-            //var timeout = DateUtils.AsString(ViewModel.Options.Timeout, format: DateUtils.Minute);
-            //var timeoutTitle = string.Concat(prefixTimeoutTitle, " [", timeout, " min]");
-            //label.Content = timeoutTitle;
-            //args.Handled = true;
-        }
-
-        private void OnParallelProcessUpdated(object sender, DataTransferEventArgs args)
-        {
-            //if (!(sender is Label label)) { return; }
-
-            //label.Content = string.Concat(prefixParallelProcess, " [", ViewModel.ParallelProcess, "]");
-            //args.Handled = true;
-        }
     }
 }
