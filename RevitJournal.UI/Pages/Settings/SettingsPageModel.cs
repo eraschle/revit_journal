28533e449990d71469a68b0225600fe8f0bd70d5
eraschle <erichraschle@gmail.com>
2020-10-08 00:08:25 +0200

    implemented events to control visibility of user controls

--- RevitJournal.UI/Pages/Settings/SettingsPageModel.cs
+++ RevitJournal.UI/Pages/Settings/SettingsPageModel.cs
@@ -5,6 +5,10 @@ using System;
 using System.Collections.ObjectModel;
 using Utilities.UI;
 using DataSource.Model.FileSystem;
+using System.Windows;
+using System.Drawing;
+using Utilities.System;
+using System.ComponentModel;
 
 namespace RevitJournalUI.Pages.Settings
 {
@@ -17,18 +21,28 @@ namespace RevitJournalUI.Pages.Settings
             FamilyDirectory = new OptionDirectoryViewModel("Family Directory", Options.RootDirectory, true);
             JournalDirectory = new OptionDirectoryViewModel("Journal Directory", Options.JournalDirectory, true);
             ActionDirectory = new OptionDirectoryViewModel("Action Directory", Options.ActionDirectory, true);
-            
-            NewRootPath = new OptionDirectoryViewModel("New Root Directory", Options.NewRootPath, false, Options.RootDirectory);
-            BackupFolder = new OptionStringViewModel("Backup Folder", Options.BackupFolder, false);
-            AddBackupAtEnd = new OptionBoolViewModel("Add folder at the end", Options.AddBackupAtEnd, true);
-            FileSuffix = new OptionStringViewModel("File Suffix", Options.FileSuffix, false);
 
             DeleteAppBackups = new OptionBoolViewModel("Delete Revit Backups", Options.DeleteRevitBackup, true);
             SourceBackup = new OptionBoolViewModel("Create source Backups", Options.CreateSourceBackup, true);
+            SourceBackup.PropertyChanged += SourceBackup_PropertyChanged;
 
-            LogResult = new OptionBoolViewModel ("Log Results",Options.LogResults, true);
-            LogSuccess = new OptionBoolViewModel ("Log succes", Options.LogSuccess, true);
-            LogError = new OptionBoolViewModel ("Log error", Options.LogError, true);
+            NewRootPath = new OptionDirectoryViewModel("New Root Directory", Options.NewRootPath, false, Options.RootDirectory);
+            NewRootPath.PropertyChanged += PathOptions_PropertyChanged;
+            BackupFolder = new OptionStringViewModel("Backup Folder", Options.BackupFolder, false);
+            BackupFolder.PropertyChanged += PathOptions_PropertyChanged;
+            BackupFolder.PropertyChanged += BackupFolder_PropertyChanged;
+            AddBackupAtEnd = new OptionBoolViewModel("Add folder at the end", Options.AddBackupAtEnd, true);
+            AddBackupAtEnd.PropertyChanged += PathOptions_PropertyChanged;
+            FileSuffix = new OptionStringViewModel("File Suffix", Options.FileSuffix, false);
+            FileSuffix.PropertyChanged += PathOptions_PropertyChanged;
+            SetBackupOptionVisibility(SourceBackup.Value);
+            SetAddFolderAtEndEnabled();
+
+            LogResult = new OptionBoolViewModel("Log Results", Options.LogResults, true);
+            LogResult.PropertyChanged += LogResult_PropertyChanged;
+            LogSuccess = new OptionBoolViewModel("Log succes", Options.LogSuccess, true);
+            LogError = new OptionBoolViewModel("Log error", Options.LogError, true);
+            SetOptionLogEnabled(LogResult.Value);
 
             ParallelProcess = new OptionSliderViewModel("Processes", Options.Processes, true);
             ProcessTimeout = new OptionSliderViewModel("Timeout", Options.ProcessTime, true, "min");
@@ -74,23 +88,95 @@ namespace RevitJournalUI.Pages.Settings
 
         public OptionSliderViewModel ProcessTimeout { get; }
 
+        #region Loggin results
+
+        private void LogResult_PropertyChanged(object sender, PropertyChangedEventArgs args)
+        {
+            if (StringUtils.Equals(args.PropertyName, nameof(LogResult.Value)) == false) { return; }
+
+            SetOptionLogEnabled(LogResult.Value);
+        }
+
+        private void SetOptionLogEnabled(bool isChecked)
+        {
+            var visibility = GetVisibility(isChecked);
+            LogError.OptionVisibility = visibility;
+            LogSuccess.OptionVisibility = visibility;
+        }
+
         public OptionBoolViewModel LogResult { get; }
 
         public OptionBoolViewModel LogSuccess { get; }
 
         public OptionBoolViewModel LogError { get; }
 
+        #endregion
+
+
+        private static Visibility GetVisibility(bool isChecked)
+        {
+            return isChecked ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        #region Backup File
+
         public OptionBoolViewModel DeleteAppBackups { get; }
 
-        #region Backup Source File
+        private void SourceBackup_PropertyChanged(object sender, PropertyChangedEventArgs args)
+        {
+            if (StringUtils.Equals(args.PropertyName, nameof(SourceBackup.Value)) == false) { return; }
+
+            SetBackupOptionVisibility(SourceBackup.Value);
+        }
+
+        private void PathOptions_PropertyChanged(object sender, PropertyChangedEventArgs args)
+        {
+            if (StringUtils.Equals(args.PropertyName, nameof(SourceBackup.Value)) == false) { return; }
+
+            SymbolicPath = string.Empty;
+        }
+
+        public Visibility BackupVisibility
+        {
+            get { return GetVisibility(SourceBackup.Value); }
+            set { NotifyPropertyChanged(); }
+        }
+
+        private void SetBackupOptionVisibility(bool isChecked)
+        {
+            var visibility = GetVisibility(isChecked);
+            NewRootPath.OptionVisibility = visibility;
+            AddBackupAtEnd.OptionVisibility = visibility;
+            BackupFolder.OptionVisibility = visibility;
+            FileSuffix.OptionVisibility = visibility;
+            BackupVisibility = visibility;
+        }
+
+        public string SymbolicPath
+        {
+            get { return Options.PathCreator.CreateSymbolic(); }
+            set { NotifyPropertyChanged(); }
+        }
 
         public OptionBoolViewModel SourceBackup { get; }
 
         public OptionDirectoryViewModel NewRootPath { get; }
 
-        public OptionBoolViewModel AddBackupAtEnd { get; }
-
         public OptionStringViewModel BackupFolder { get; }
+
+        private void BackupFolder_PropertyChanged(object sender, PropertyChangedEventArgs args)
+        {
+            if (StringUtils.Equals(args.PropertyName, nameof(BackupFolder.Value)) == false) { return; }
+
+            SetAddFolderAtEndEnabled();
+        }
+
+        private void SetAddFolderAtEndEnabled()
+        {
+            AddBackupAtEnd.IsEnabled = string.IsNullOrWhiteSpace(BackupFolder.Value) == false;
+        }
+
+        public OptionBoolViewModel AddBackupAtEnd { get; }
 
         public OptionStringViewModel FileSuffix { get; }
 
