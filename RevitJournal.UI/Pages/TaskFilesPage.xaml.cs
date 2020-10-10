e479df49ee99bf58bbd890aa6e0ce3b1ceeb315c
eraschle <erichraschle@gmail.com>
2020-10-11 01:05:52 +0200

    start implemented TaskFilesPage

--- RevitJournal.UI/Pages/TaskFilesPage.xaml.cs
+++ RevitJournal.UI/Pages/TaskFilesPage.xaml.cs
@@ -1,4 +1,9 @@
-﻿using System.Windows.Controls;
+﻿using RevitJournal.Revit.Filtering;
+using RevitJournalUI.JournalTaskUI;
+using RevitJournalUI.JournalTaskUI.Models;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
 
 namespace RevitJournalUI.Pages
 {
@@ -7,9 +12,40 @@ namespace RevitJournalUI.Pages
     /// </summary>
     public partial class TaskFilesPage : Page
     {
+        private FamilyOverviewViewModel ViewModel
+        {
+            get { return DataContext as FamilyOverviewViewModel; }
+        }
+
         public TaskFilesPage()
         {
             InitializeComponent();
+        }
+
+        private void TreeView_SelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> args)
+        {
+            if (args.NewValue is DirectoryViewModel newModel)
+            {
+                ViewModel.SelectedDirectory = newModel;
+                ViewModel.UpdateFamilyViewModels(newModel);
+                args.Handled = true;
+            }
+        }
+
+        private void DirectoryFilter(object sender, FilterEventArgs args)
+        {
+            if (!(args.Item is DirectoryViewModel model)) { return; }
+
+            var folder = model.Handler.Folder;
+            args.Accepted &= RevitFilterManager.Instance.DirectoryFilter(folder);
+        }
+
+        private void FileFilter(object sender, FilterEventArgs args)
+        {
+            if (!(args.Item is FamilyViewModel model)) { return; }
+
+            var file = model.Handler.File;
+            args.Accepted &= RevitFilterManager.Instance.FileFilter(file);
         }
     }
 }
